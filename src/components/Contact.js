c12b1dec05eeb49154cbf493a64bfbfdfe9cffdb
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-14 17:41:50 +0200

    Transformed Contact component into class with state and made status changed on clic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,32 @@
 import React from "react";
 import "./Contact.css";
 
-function Contact(props) {
-  return (
-    <div >
-    <div key={props.id} className="Contact"> 
-      <img className="avatar" src={props.imgSrc} alt="Avatar" />
+class Contact extends React.Component {
+
+  state = {
+    online: this.props.isOnline
+  }
+
+  onHandleClick = () => {
+    this.setState({online: !this.state.online})
+    console.log(this.state.online);
+  }
+  render () {
+    return (
+      <div onClick={this.onHandleClick} >
+    <div key={this.props.id} className="Contact"> 
+      <img className="avatar" src={this.props.imgSrc} alt="Avatar" />
       <div>
-        <p className="name">{props.name}</p>
+        <p className="name">{this.props.name}</p>
 
         <div className="status flex">
           <span
             className={
-              props.isOnline ? "status-online" : "status-offline"
+              this.state.online ? "status-online" : "status-offline"
             }
           ></span>
           <p className="status-text">
-            {props.isOnline ? "Online" : "Offline"}
+            {this.state.online ? "Online" : "Offline"}
            
           </p>
         </div>
@@ -26,4 +36,5 @@ function Contact(props) {
   );
 }
 
+} 
 export default Contact;
